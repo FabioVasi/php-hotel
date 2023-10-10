b31d14ad6e001f185d2cd5b19232cf5797e37bc8
FabioVasi <vasifabio96@gmail.com>
2023-10-10 17:33:32 +0200

    refactoring table end ex

--- index.php
+++ index.php
@@ -86,47 +86,25 @@ $hotels = [
                 <table class="table border border-2 border-primary mt-5">
                     <thead>
                         <tr>
-                            <th scope="col-2">Hotels</th>
-                            <th class="border-start border-primary" scope="col-2">1</th>
-                            <th class="border-start border-primary" scope="col-2">2</th>
-                            <th class="border-start border-primary" scope="col-2">3</th>
-                            <th class="border-start border-primary" scope="col-2">4</th>
-                            <th class="border-start border-primary" scope="col-2">5</th>
+                            <th class="border-start border-primary" scope="col">Name</th>
+                            <th class="border-start border-primary" scope="col">Description</th>
+                            <th class="border-start border-primary" scope="col">Parking</th>
+                            <th class="border-start border-primary" scope="col">Vote</th>
+                            <th class="border-start border-primary" scope="col">Distance to center</th>
                         </tr>
                     </thead>
                     <tbody class="border border-primary">
-                        <tr>
-                            <th scope="row">Name</th>
-                            <?php foreach($hotels as $hotel) : ?>
+                        <?php foreach($hotels as $hotel) : ?>
+                            <tr>
                                 <td class="border-start border-primary"><?= $hotel['name'] ?></td>
-                            <?php endforeach; ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Description</th>
-                            <?php foreach($hotels as $hotel) : ?>
                                 <td class="border-start border-primary"><?= $hotel['description'] ?></td>
-                            <?php endforeach; ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Parking</th>
-                            <?php foreach($hotels as $hotel) : ?>
                                 <td class="border-start border-primary"><?= $hotel['parking'] ?></td>
-                            <?php endforeach; ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Vote</th>
-                            <?php foreach($hotels as $hotel) : ?>
-                                <td class="border-start border-primary">
+                                <td class="border-start border-primary d-flex justify-content-start align-items-center">
                                     <?= $hotel['vote'] ?>/5 <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
                                 </td>
-                            <?php endforeach; ?>
-                        </tr>
-                        <tr>
-                            <th scope="row">Distance to center</th>
-                            <?php foreach($hotels as $hotel) : ?>
                                 <td class="border-start border-primary"><?= $hotel['distance_to_center'] ?> km</td>
-                            <?php endforeach; ?>
-                        </tr>
+                            </tr>
+                        <?php endforeach; ?>    
                     </tbody>
                 </table>
             </div>
